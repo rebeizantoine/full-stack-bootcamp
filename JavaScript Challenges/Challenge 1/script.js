08da8eaba8bcf1e116635dbac8813ec7fca1305c
Antoine Rebeiz <rebeizantoine@gmail.com>
2023-08-17 10:39:54 +0300

    Challenge 1 - Done with Step 4

--- JavaScript Challenges/Challenge 1/script.js
+++ JavaScript Challenges/Challenge 1/script.js
@@ -8,9 +8,26 @@ const lastName = "Rebeiz";
 let DOB;
 
 age = 23;
-lastName = "Rebeiz";
+
 DOB = "22032000";
 
 console.log("My age is: " + age);
 console.log("My lastName is:" + lastName);
 console.log("My DOB is: " + DOB);
+
+
+console.log("This is a text");
+console.log(true);
+console.log(false);
+console.log(1215);
+console.log({});
+console.log([]);
+console.log(null);
+console.log(undefined);
+
+let text = "this is a text";
+let boolean = true;
+let number = 1215;
+let array = [];
+let string = "this is string";
+let object = null;
