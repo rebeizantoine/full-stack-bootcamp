a920c1727327cc7700e07733f7276e5e3d834b79
Antoine Rebeiz <rebeizantoine@gmail.com>
2023-08-17 10:26:32 +0300

    Challenge 1 - Done with Step 3

--- JavaScript Challenges/Challenge 1/script.js
+++ JavaScript Challenges/Challenge 1/script.js
@@ -1,1 +1,16 @@
 console.log('I am linked now!');
+var name;
+name = "ANTOINE";
+alert("My name is: " + name)
+
+var age;
+const lastName = "Rebeiz";
+let DOB;
+
+age = 23;
+lastName = "Rebeiz";
+DOB = "22032000";
+
+console.log("My age is: " + age);
+console.log("My lastName is:" + lastName);
+console.log("My DOB is: " + DOB);
