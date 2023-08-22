fa5ffbcb6453a57075102c57ffc39bec7918e6af
Antoine Rebeiz <rebeizantoine@gmail.com>
2023-08-22 21:10:56 +0300

    Done with challenge 4

---
+++ JavaScript Challenges/Challenge 4/script.js
@@ -0,0 +1,58 @@
+//STEP1
+
+// Declare and initialize the array
+const fiveNumbers = [1, 2, 3, 4, 5];
+
+// Construct the object
+const personalInfo = {
+    name: "Antoine",
+    age: 23,
+    height: "186cm",
+    location: "Jeita",
+    nationality: "Lebanese"
+};
+//STEP2
+
+
+const nextThreeNumbers = [6, 7, 8];
+
+
+const allNumbers = [...fiveNumbers, ...nextThreeNumbers];
+
+
+const additionalInfo = {
+    occupation: "Engineer",
+    hobby: "Chess",
+    education: "Master's in Civil engineering"
+};
+
+// Use the spread operator to merge objects
+// l 3 point la t7ottoun jouwa w , baynetoun
+const fullInfo = { ...personalInfo, ...additionalInfo };
+
+
+// Destructure first three numbers from the array
+//jib l 3 numbers men fivenumbers
+const [firstNum, secondNum, thirdNum] = fiveNumbers;
+
+// Destructure properties from the personalInfo object
+//same with this
+const { name, location, nationality } = personalInfo;
+
+//STEP4
+//bel aakes now
+const personalInfoWithContact = {
+    ...personalInfo,
+    contactInfo: {
+        phone: "123-456-7890",
+        email: "john@example.com"
+    }
+};
+
+// Rest with destructuring to extract first two numbers and rest
+const [num1, num2, ...restOfNumbers] = fiveNumbers;
+
+// Merging and overwriting with spread
+const object1 = { a: 1, b: 2 };
+const object2 = { b: 3, c: 4 };
+const mergedObjects = { ...object1, ...object2 };
